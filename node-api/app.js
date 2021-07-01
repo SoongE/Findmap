37c2d67e4f0996e0f710b6c6b31f676461937373
rhcsky <dev.rhcsky@gmail.com>
2021-07-01 14:05:19 +0000

    Remove user instance

--- node-api/app.js
+++ node-api/app.js
@@ -6,7 +6,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var testRouter = require('./routes/test')
+// var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -21,7 +22,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/test', testRouter)
+// app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- node-api/routes/test.js
+++ node-api/routes/test.js
@@ -3,9 +3,12 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Test' });
 });
 
-// router.use('/users', require('./users'));
+router.get('/name', function(req,res,next) {
+    var name = req.query.name;
+    res.send(name);
+});
 
 module.exports = router;
