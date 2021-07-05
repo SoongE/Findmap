d8ba8a33366efac9571ff8c797dc3b5d0aba188f
rhcsky <dev.rhcsky@gmail.com>
2021-07-06 07:22:39 +0900

    Implement basic call

---
+++ flask-api/app.py
@@ -0,0 +1,17 @@
+from flask import Flask, escape, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Flask Server'
+
+@app.route('/whoyouare')
+def whoyouare():
+    print('hello')
+    name = request.args.get("name")
+
+    return {"message" : f'FLASK SEND: Hello, {name}!'}
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
