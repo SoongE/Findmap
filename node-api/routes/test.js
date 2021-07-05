d054b6a35dd1c40d9251792a6419c067366afecd
rhcsky <dev.rhcsky@gmail.com>
2021-07-06 07:23:26 +0900

    Implement axios

--- node-api/routes/test.js
+++ node-api/routes/test.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Test' });
+router.get('/', (req, res, next) => {
+  axios.get('http://flask-api:5000/whoyouare',{ params: { 'name': "nodeJS" } })
+  .then(response=>res.send(response.data))
+  .catch(error=>res.send(error.message))
+  .finally();
 });
 
 router.get('/name', function(req,res,next) {
