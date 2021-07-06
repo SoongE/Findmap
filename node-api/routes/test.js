e3851905f9686d0cb66a9aa1eaa7488438086fbd
rhcsky <dev.rhcsky@gmail.com>
2021-07-06 09:01:08 +0900

    Implement flask request call

--- node-api/routes/test.js
+++ node-api/routes/test.js
@@ -4,15 +4,17 @@ const axios = require('axios');
 
 /* GET home page. */
 router.get('/', (req, res, next) => {
-  axios.get('http://flask-api:5000/whoyouare',{ params: { 'name': "nodeJS" } })
+  axios.get('http://flask-api:5000/test')
   .then(response=>res.send(response.data))
   .catch(error=>res.send(error.message))
   .finally();
 });
 
-router.get('/name', function(req,res,next) {
-    var name = req.query.name;
-    res.send(name);
+router.get('/name', (req, res, next) => {
+  axios.get('http://flask-api:5000/test/name',{ params: { 'name': "nodeJS" } })
+  .then(response=>res.send(response.data))
+  .catch(error=>res.send(error.message))
+  .finally();
 });
 
 module.exports = router;
