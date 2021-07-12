f93360d3152353cda26b36228c91cadcbb35b331
Ubuntu <ubuntu@ip-172-31-7-103.ap-northeast-2.compute.internal>
2021-07-12 12:35:27 +0000

    modify routes

--- node-api/app.js
+++ node-api/app.js
@@ -8,6 +8,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var testRouter = require('./routes/test');
 var usersRouter = require('./routes/users');
+var scrapRouter = require('./routes/scrap');
 
 var app = express();
 
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/test', testRouter);
 app.use('/users', usersRouter);
+app.use('/scrap',scrapRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
