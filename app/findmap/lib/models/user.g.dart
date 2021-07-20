1bcee322a5d9b8fa9a3b24f4f1837e80c37f6dcf
오승민 <dev.rhcsky@gmail.com>
2021-07-20 22:06:12 +0900

    Update user model

--- app/findmap/lib/models/user.g.dart
+++ app/findmap/lib/models/user.g.dart
@@ -8,12 +8,28 @@ part of 'user.dart';
 
 User _$UserFromJson(Map<String, dynamic> json) {
   return User(
+    json['userIdx'] as int,
+    json['accessToken'] as String,
     json['name'] as String,
     json['email'] as String,
+    json['password'] as String,
+    json['nickName'] as String,
+    json['birthday'] as String,
+    json['gender'] as String,
+    json['phoneNum'] as String,
+    json['taste'] as String,
   );
 }
 
 Map<String, dynamic> _$UserToJson(User instance) => <String, dynamic>{
+      'userIdx': instance.userIdx,
+      'accessToken': instance.accessToken,
       'name': instance.name,
       'email': instance.email,
+      'password': instance.password,
+      'nickName': instance.nickName,
+      'birthday': instance.birthday,
+      'gender': instance.gender,
+      'phoneNum': instance.phoneNum,
+      'taste': instance.taste,
     };
