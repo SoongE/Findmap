f14c123bf1e92b7cb04eaf77b770b6a209eefacb
Jiyun Park <jiyoon0043@ajou.ac.kr>
2021-07-25 17:44:24 +0000

    FEAT: use open Graph Tags when crawl the page

--- NLP/jiyun/main.py
+++ NLP/jiyun/main.py
@@ -1,6 +1,5 @@
 import crawler
 import model
-import pymysql
 
 def main():
     # temporary url. It will get the current url from nodejs later.
@@ -21,7 +20,10 @@ def main():
 
         if title is not None:
             try:
-                category_list = ['문학', '영화', '미술', '공연', '전시', '음악', '드라마', '스타', '만화', '방송', '일상', '육아', '결혼', '반려동물', '패션', '인테리어', '요리', '리뷰', '원예', '게임', '스포츠', '사진', '자동차', '취미', '국내여행', '세계여행', '맛집', 'IT', '사회', '정치', '건강', '의학', '경제', '외국어', '교육', '학문']
+                category_list = ['문학/책', '영화', '미술/디자인', '공연/전시', '음악', '드라마', '스타/연예인', '만화/애니',
+                                 '방송', '일상/생각', '육아/결혼', '애완/반려동물', '좋은글/이미지', '패션/미용', '인테리어/DIY',
+                                 '요리/레시피', '상품리뷰', '원예/재배', '게임', '스포츠', '사진', '자동차', '취미', '국내여행',
+                                 '세계여행', '맛집', 'IT/컴퓨터', '사회/정치', '건강/의학', '비즈니스/경제', '외학/외국어', '교육/학문']
                 category_predict = nlp.categorize(title, category_list)
                 scrap_page['category'] = max(category_predict, key=category_predict.get)
             except:
@@ -38,21 +40,8 @@ def main():
         scrap_page['url'] = url
         scrap_page['title'] = None
         scrap_page['summary'] = None
-        scrap_page['img_url'] = "default"
+        scrap_page['img_url'] = None
 
-    # connect DB and insert "scrap_page" into ScrapTB
-    conn = pymysql.connect(host='findmap-first-db.c2jag33neij8.ap-northeast-2.rds.amazonaws.com',
-                           port=3306,
-                           user='admin',
-                           password='mypassword',
-                           db='findmap-first-db',
-                           charset='utf8')
-    cur = conn.cursor()
-    query = "INSERT INTO ScrapTB(idx, userIdx, title, contentUrl, thumbnailUrl, summary, comment, folderIdx, feedIdx) " \
-          "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s);"
-    cur.execute(query, (4, 1, scrap_page['title'], scrap_page['url'], scrap_page['img_url'], scrap_page['summary'], "COMMENT", 1, 1))
-    conn.commit()
-    conn.close()
 
 if __name__ == '__main__':
     main()
