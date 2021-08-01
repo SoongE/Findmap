5aba88bc30a930f001acf46fe1b29e541e476c2d
오승민 <dev.rhcsky@gmail.com>
2021-08-02 07:14:20 +0900

    Create post model

---
+++ app/findmap/lib/models/post.dart
@@ -0,0 +1,22 @@
+import 'package:json_annotation/json_annotation.dart';
+
+part 'post.g.dart';
+
+@JsonSerializable()
+class Post {
+  int idx;
+  String title;
+  String contentUrl;
+  String thumbnailUrl;
+  String summary;
+  String comment;
+  int categoryIdx;
+  int folderIdx;
+
+  Post(this.idx, this.title, this.contentUrl, this.thumbnailUrl, this.summary,
+      this.comment, this.categoryIdx, this.folderIdx);
+
+  factory Post.fromJson(Map<String, dynamic> json) => _$PostFromJson(json);
+
+  Map<String, dynamic> toJson() => _$PostToJson(this);
+}
