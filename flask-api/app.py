da4a05bc6eb93ffcde6eb4b6364fa5a78d4d2abb
Hyeon Su <tmdgustn06@naver.com>
2021-10-07 20:00:55 +0900

    testing

--- flask-api/app.py
+++ flask-api/app.py
@@ -3,7 +3,7 @@ import fasttext
 
 app = create_app("dev")
 app.app_context().push()
-model = fasttext.load_model('./morphnamunaver.bin')
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
