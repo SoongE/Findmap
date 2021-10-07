3ce562e3d28ccf195805a68f02da070de0369ae0
Hyeon Su <tmdgustn06@naver.com>
2021-10-07 18:52:34 +0900

    ++jade

--- node-api/routes/test.js
+++ node-api/routes/test.js
@@ -16,6 +16,13 @@ router.get('/name', (req, res, next) => {
   .catch(error=>res.send(error.message))
   .finally();
 });
+router.get('/recommend', (req, res, next) => {
+  axios.get('http://flask-api:5000/recommend')
+  .then(response=>res.send(response.data))
+  .catch(error=>res.send(error.message))
+  .finally();
+});
+
 
 const body = { 'title': 'Axios POST Request Example' };
 router.get('/post', (req,res,next) =>{
