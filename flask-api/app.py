e124f4c48269d6ad927167cf8d6fcf933530d6bb
승현수 <tmdgustn06@naver.com>
2021-10-09 19:04:36 +0900

    /

--- flask-api/app.py
+++ flask-api/app.py
@@ -1,9 +1,9 @@
-from main.runner import create_app
+from main import runner
 import fasttext
 
 mod = fasttext.load_model('.recommend/morphnamunaver.bin')
 
-app = create_app("dev")
+app = runner.create_app("dev")
 app.app_context().push()
 
 if __name__ == "__main__":
