cf02ae417bc940f90bdc4a6e6b85b5aee338e457
승현수 <tmdgustn06@naver.com>
2021-10-09 17:57:18 +0900

    splited

--- flask-api/main/runner.py
+++ flask-api/main/runner.py
@@ -7,8 +7,6 @@ from flask import Flask, url_for, redirect, render_template, request, abort
 from main.config import config_by_name
 from main.api_url import api_urls
 
-
-
 def create_app(config_name):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
