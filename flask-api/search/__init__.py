3e04320469bb32534baf33147afb2561a6c08bf8
승현수 <tmdgustn06@naver.com>
2021-10-13 14:13:17 +0900

    ++api

--- flask-api/search/__init__.py
+++ flask-api/search/__init__.py
@@ -12,14 +12,26 @@ FAILURE = "failure"
 
 @search_api.route('/')
 def main():
-    mp = main_method.Mainmethod()
+    param_dict = request.args.to_dict()
+    param_value = NULL
+
+    for key in param_dict.keys() :
+      param_value = request.args[key]#### 검색어 param
+
+    mp = main_method.Mainmethod(param_value)
     search_list = mp.main()
     resources = {"search_html" : search_list}
     return make_response(SUCCESS,resources)
 
 @search_api.route('/categorize',)
 def categorize():
-    search_idx_cl = model.Categorize("아이언맨")
+    param_dict = request.args.to_dict()
+    param_value = NULL
+
+    for key in param_dict.keys() :
+      param_value = request.args[key]#### 검색어 param
+
+    search_idx_cl = model.Categorize(param_value)
     search_idx = search_idx_cl.ctg()
     body = {"ctg" : search_idx}
     return make_response(SUCCESS, body)
