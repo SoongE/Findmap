2bfdae1beec69ebec7b855bc9ae66525d639d403
승현수 <tmdgustn06@naver.com>
2021-10-13 12:41:13 +0900

    ++api

--- flask-api/search/__init__.py
+++ flask-api/search/__init__.py
@@ -14,20 +14,13 @@ FAILURE = "failure"
 def main():
     mp = main_method.Mainmethod()
     search_list = mp.main()
-    search_idx_cl = model.Categorize("아이언맨")
-    search_idx = search_idx_cl.ctg()
-    resources = {"search_html" : search_list, "ctg" : search_idx}
+    resources = {"search_html" : search_list}
     return make_response(SUCCESS,resources)
 
-@search_api.route('/name', methods=['GET'])
-def name():
-    name = request.args["name"]
-    body = {"message" : f'FLASK SEND: Hello, {name}!'}
+@search_api.route('/categorize',)
+def categorize():
+    search_idx_cl = model.Categorize("아이언맨")
+    search_idx = search_idx_cl.ctg()
+    body = {"ctg" : search_idx}
     return make_response(SUCCESS, body)
 
-@search_api.route('/post', methods=['POST'])
-def post():
-    data =  request.get_json()
-    print(f"title: {data['title']}")
-    body = {"log" : "Check your flask log"}
-    return make_response(SUCCESS, body)
