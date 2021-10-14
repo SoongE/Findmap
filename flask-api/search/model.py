448f0d008e633374a56021583e66793174b64f77
Hyeon Su <tmdgustn06@naver.com>
2021-10-15 03:58:27 +0900

    ++

--- flask-api/search/model.py
+++ flask-api/search/model.py
@@ -10,24 +10,24 @@ class Categorize :
                                      #'요리/레시피', '상품리뷰', '원예/재배', '게임', '스포츠', '사진', '자동차', '취미', '국내여행',
                                      #'세계여행', '맛집', 'IT/컴퓨터', '사회/정치', '건강/의학', '비즈니스/경제', '외학/외국어', '교육/학문']
         
-         host_name = "findmap-first-db.c2jag33neij8.ap-northeast-2.rds.amazonaws.com"
-         user_name = "admin"
-         password = "mypassword"
-         db_name = "findmap-first-db"
+        host_name = "findmap-first-db.c2jag33neij8.ap-northeast-2.rds.amazonaws.com"
+        user_name = "admin"
+        password = "mypassword"
+        db_name = "findmap-first-db"
 
 
-         db = pymysql.connect(
+        db = pymysql.connect(
            host = host_name,
            port = 3306,
            user = user_name,
            passwd = password,
            db = db_name,
            charset = 'utf8'
-         )
+        )
 
-         SQL = " select * from CategoryTB"
+        SQL = " select * from CategoryTB"
 
-         self.df  = pd.read_sql(SQL,db)
+        self.df  = pd.read_sql(SQL,db)
     
     def ctg(self):
         smr = Pororo(task="similarity", lang="ko")
