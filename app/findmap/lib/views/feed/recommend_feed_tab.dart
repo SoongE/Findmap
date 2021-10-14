674eeed198d2634859b4ee9950f3d1a814d020a7
Rhcsky <dev.rhcsky@gmail.com>
2021-10-15 05:56:39 +0900

    Change params

--- app/findmap/lib/views/feed/recommend_feed_tab.dart
+++ app/findmap/lib/views/feed/recommend_feed_tab.dart
@@ -224,8 +224,7 @@ class _RecommendFeedTabState extends State<RecommendFeedTab>
   Future<List<Feed>> fetchGetRecommendFeeds(BuildContext context) async {
     final response = await http.get(
       // TODO change to feeds/recommend
-      Uri.http(BASEURL, '/feeds/recommendation',
-          {'scrapIdxList': '1,2,3,4,5,6,7,8'}),
+      Uri.http(BASEURL, '/feeds/recommendation', {'scrapIdxList': '2,3,4,5'}),
       headers: {
         HttpHeaders.contentTypeHeader: "application/json",
         "token": widget.user.accessToken,
