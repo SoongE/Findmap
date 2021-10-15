db036f964e3bb29d16383b47d36e43af3b7e8f70
오승민 <dev.rhcsky@gmail.com>
2021-10-15 01:41:53 +0000

    Update flask

--- flask-api/search/main_method.py
+++ flask-api/search/main_method.py
@@ -3,84 +3,53 @@ from search import model
 from search import crawler
 
 class Mainmethod :
-  def __init__(self,search_idx) :
-    self.searcher = searcher.Searcher()
-    self.result = list()
-    self.search = search_idx
-  def main(self):
-    search_text = self.search # 추후 노드에서 받을 예정!
-    
-    # 검색 결과의 title 로부터 카테고리를 유추
-    mod = model.Categorize(search_text)
-    naver_result = self.searcher.naver_get_result(search_text)
-    kakao_result = self.searcher.kakao_get_result(search_text)
+    def __init__(self,search_idx) :
+        self.searcher = searcher.Searcher()
+        self.result = list()
+        self.search = search_idx
+    def main(self):
+        search_text = self.search # 추후 노드에서 받을 예정!
+        
+        # 검색 결과의 title 로부터 카테고리를 유추
+        mod = model.Categorize(search_text)
+        naver_result = self.searcher.naver_get_result(search_text)
+        kakao_result = self.searcher.kakao_get_result(search_text)
 
-    if not naver_result and not kakao_result:
-        # naver와 kakao 모두 rest api 통신 실패
-        # node.js 에 에러 던져줌
-        exit(0)
+        if not naver_result and not kakao_result:
+            # naver와 kakao 모두 rest api 통신 실패
+            # node.js 에 에러 던져줌
+            exit(0)
 
-    elif not naver_result:
-        # naver 통신 과정 중 에러 발생
-        # kakao에서 얻어온 정보로만 사용자 검색 결과 추천
-        self.result = kakao_result
+        elif not naver_result:
+            # naver 통신 과정 중 에러 발생
+            # kakao에서 얻어온 정보로만 사용자 검색 결과 추천
+            self.result = kakao_result
 
-    elif not kakao_result:
-        # kakao 통신 과정 중 에러 발생
-        # naver 에서 얻어온 정보로만 사용자 검색 결과 추천
-        self.result = naver_result
-    else:
-        # naver와 kakao 모두 정상적으로 통신
-        self.result = naver_result + kakao_result
-    
+        elif not kakao_result:
+            # kakao 통신 과정 중 에러 발생
+            # naver 에서 얻어온 정보로만 사용자 검색 결과 추천
+            self.result = naver_result
+        else:
+            # naver와 kakao 모두 정상적으로 통신
+            self.result = naver_result + kakao_result
+        
 
-    # node.js 로부터 사용자 정보 받아오기.
-    # 각 카테고리별 스크랩 수 / 최초 관심사 카테고리 등등...
+        # node.js 로부터 사용자 정보 받아오기.
+        # 각 카테고리별 스크랩 수 / 최초 관심사 카테고리 등등...
 
 
-    mod.categorize(self.result)
+        mod.categorize(self.result)
 
-    # node.js 로부터 사용자 정보 받아오기.
-    # 각 카테고리별 스크랩 수 / 최초 관심사 카테고리 등등...
+        # node.js 로부터 사용자 정보 받아오기.
+        # 각 카테고리별 스크랩 수 / 최초 관심사 카테고리 등등...
 
-    result = sorted(self.result, key=lambda content: (-content['pred'], content['title']))
-    for x in result:
-        print(f'\n{x["title"]}\n{x["link"]}\n{x["description"]}\n')
+        result = sorted(self.result, key=lambda content: (-content['pred'], content['title']))
+        for x in result:
+            print(f'\n{x["title"]}\n{x["link"]}\n{x["description"]}\n')
 
-    # result = sorted(self.result, key=lambda content: (content['title']))
-    
-    return result
-
-def share(url):
-    # temporary url. It will get the current url from nodejs later.
-    crw = crawler.Crawler(url)
-    nlp = model.PororoModel()
-
-    try:
-        if crw.robots_check():
-            # this page admit crawling
-            scrap_page = crw.crawl()
-
-            title = scrap_page['title']
-
-            if 'sentences' in scrap_page:
-                sentences = scrap_page['sentences']
-                if sentences is not None:
-                    description = nlp.summarize(sentences)
-                    scrap_page['description'] = description
-
-            if title is not None:
-                try:
-                    category_list = ['문학/책', '영화', '미술/디자인', '공연/전시', '음악', '드라마', '스타/연예인', '만화/애니',
-                                     '방송', '일상/생각', '육아/결혼', '애완/반려동물', '좋은글/이미지', '패션/미용', '인테리어/DIY',
-                                     '요리/레시피', '상품리뷰', '원예/재배', '게임', '스포츠', '사진', '자동차', '취미', '국내여행',
-                                     '세계여행', '맛집', 'IT/컴퓨터', '사회/정치', '건강/의학', '비즈니스/경제', '외학/외국어', '교육/학문']
-                    category_predict = nlp.categorize(title, category_list)
-                    scrap_page['category'] = max(category_predict, key=category_predict.get)
-                except:
-                    scrap_page['category'] = None
-            else:
-                scrap_page['category'] = None
+        # result = sorted(self.result, key=lambda content: (content['title']))
+        
+        return result
 
 def share(url):
     # temporary url. It will get the current url from nodejs later.
