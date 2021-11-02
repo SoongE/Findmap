1eec9237395f70d098f6ff543ea7675d2d265afa
defwdahyun0 <wdh112139@gmail.com>
2021-11-03 01:53:05 +0900

    modified flask api

--- node-api/controllers/recommend.js
+++ node-api/controllers/recommend.js
@@ -74,12 +74,11 @@ const search = {
         .finally();
     },
     getRecommendCategorizeWord: async (req, res) => {
-        const userIdx = req.decoded.userIdx;
         const keyword = req.query.keyword
 
         if (!keyword) return res.json({success: false, code: 2701, message: "keyword(검색어)를 입력해 주세요."});
 
-        axios.get('http://flask-api:5000/search/categorize',{ params: { keyword: keyword , userIdx:userIdx} })
+        axios.get('http://flask-api:5000/search/categorize',{ params: { keyword: keyword} })
         .then(response=>res.send(response.data))
         .catch(error=>res.send(error.message))
         .finally();
